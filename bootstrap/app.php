901096ce630c4af693bfaa1d7c73f5854b4cd4b0
wuqiang <foryoufeng@gmail.com>
2025-03-20 19:51:23 +0800

    manage route change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,11 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        then: function () {
-            Route::middleware('manage')->prefix(config('app.manage_prefix'))->group(base_path('routes/manage.php'));
-        }
+        commands: __DIR__.'/../routes/console.php'
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance']);
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('manage')->prefix(config('app.manage_prefix'))->group(base_path('routes/manage.php'));
+
+//home
 Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '.*');
