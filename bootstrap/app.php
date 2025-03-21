c6de5e4b35cd89faebc4f665b6609e92143d772f
李艳松 <walkerdistance@163.com>
2025-03-21 14:36:48 +0800

    change bootstrap app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Middleware\Manage\Authenticate as ManageAuthenticate;
+use App\Enums\CustomCodeEnum;
+use App\Exceptions\BusinessException;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\Manage\Authenticate as ManageAuthenticate;
 use App\Http\Middleware\Manage\CustomStartSession;
+use App\Traits\ApiResponse;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
@@ -11,8 +14,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,6 +28,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php'
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->group('api', [
+            HandleCors::class,
+        ]);
+
         $middleware->encryptCookies(except: ['appearance']);
 
         $middleware->web(append: [
@@ -45,5 +56,38 @@ return Application::configure(basePath: dirname(__DIR__))
         ])));
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // 不记录到日志的异常
+        $exceptions->dontReport([
+            BusinessException::class,
+        ]);
+        // 封装异常返回
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $authentication_exception) {
+            $api_response = new class
+            {
+                use ApiResponse;
+            };
+
+            return $api_response->error('未登录，请先登录。', CustomCodeEnum::UNAUTHORIZED);
+        })->render(function (UnauthorizedHttpException $exception) {
+            $apiResponse = new class
+            {
+                use ApiResponse;
+            };
+
+            return $apiResponse->error('请重新登录', CustomCodeEnum::UNAUTHORIZED);
+        })->render(function (ValidationException $validation_exception) {
+            $apiResponse = new class
+            {
+                use ApiResponse;
+            };
+
+            return $apiResponse->error($validation_exception->validator->errors()->first());
+        })->render(function (UnauthorizedException $exception) {
+            $apiResponse = new class
+            {
+                use ApiResponse;
+            };
+
+            return $apiResponse->error($exception->getMessage(), CustomCodeEnum::UNAUTHORIZED);
+        });
     })->create();
