48b6de634916b6325108cb92e747107f6660ab81
李艳松 <walkerdistance@163.com>
2025-03-26 20:35:04 +0800

    change permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ShopConfigTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
         $this->call(AppWebsiteDecorationTableSeeder::class);
     }
 }
