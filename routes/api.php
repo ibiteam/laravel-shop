aaad59c0e2e2d0935b3a813b5ba7535d99061ab2
张未东 <boyphp@163.com>
2025-03-26 18:02:51 +0800

    feat(pc): 获取省市区信息

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Home\RegionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware([])->group(function () {
@@ -9,7 +10,10 @@ Route::prefix('v1')->middleware([])->group(function () {
 Route::prefix('seller')->middleware([])->group(function () {
     require __DIR__.'/api_seller.php';
 });
+
 Route::prefix('pc')->middleware([])->group(function () {
+    Route::get('region', [RegionController::class, 'getRegion']);  // 获取省市区信息
+
     require __DIR__.'/api_home.php';
 });
 
