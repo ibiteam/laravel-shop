88299b2a209a715a9804ad91d093dbcdc03caa71
李艳松 <walkerdistance@163.com>
2025-03-26 17:05:51 +0800

    add seller filesystem api interface

--- routes/api_seller.php
+++ routes/api_seller.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Seller\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Seller\UploadController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('upload', [UploadController::class, 'upload']);
     // 首页
     Route::prefix('home')->group(function () {
         Route::get('/', [DashboardController::class, 'index']);
