51ba30e1c00be8519c4942305a468379a4f79f9c
李艳松 <walkerdistance@163.com>
2025-03-27 16:00:54 +0800

    change laravel base settings

--- app/Http/Middleware/Api/Authenticate.php
+++ app/Http/Middleware/Api/Authenticate.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace App\Http\Middleware\Manage;
+namespace App\Http\Middleware\Api;
 
-use App\Services\ManageRouteService;
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as IlluminateAuthenticate;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Request;
 
 class Authenticate extends IlluminateAuthenticate
 {
     public function __construct(Auth $auth)
     {
         parent::__construct($auth);
-        // 重定向到后台登录页面
-        parent::redirectUsing(fn () => ManageRouteService::manageLogin());
+        // 重定向到首页
+        parent::redirectUsing(fn (Request $request) => '/');
     }
 
     public function handle($request, Closure $next, ...$guards)
     {
-        $this->authenticate($request, [config('auth.manage.guard')]);
+        $this->authenticate($request, [config('auth.api.guard')]);
 
         return $next($request);
     }
--- app/Services/ManageRouteService.php
+++ app/Services/ManageRouteService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 class ManageRouteService
 {
     private const MANAGE_HOME = 'manage/home';
+    private const MANAGE_LOGIN = 'manage/login';
 
     /**
      * 管理后台首页.
@@ -12,6 +13,15 @@ class ManageRouteService
     public static function manageHome(): string
     {
         return self::getHost().'/'.self::MANAGE_HOME;
+    }
+
+    /**
+     * 管理后台登录.
+     * @return string
+     */
+    public static function manageLogin(): string
+    {
+        return self::getHost().'/'.self::MANAGE_LOGIN;
     }
 
     private static function getHost(): string
