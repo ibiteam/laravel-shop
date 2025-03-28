4fe812aee144c01b60e9942828ee87ece31071a2
李艳松 <walkerdistance@163.com>
2025-03-28 11:04:48 +0800

    delete brand controller and view

--- routes/manage/goods.php
+++ routes/manage/goods.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Manage\GoodsBrandController;
 use App\Http\Controllers\Manage\GoodsCategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,12 +10,5 @@ Route::prefix('goods')->group(function () {
         Route::get('/edit', [GoodsCategoryController::class, 'edit']); // 商品分类编辑
         Route::post('/update', [GoodsCategoryController::class, 'update']); // 商品分类更新(新增)
         Route::post('/destroy', [GoodsCategoryController::class, 'destroy']); // 商品分类删除
-    });
-    // 商品品牌
-    Route::prefix('brand')->group(function () {
-        Route::get('/', [GoodsBrandController::class, 'index']); // 商品品牌列表
-        Route::get('/edit', [GoodsBrandController::class, 'edit']); // 商品品牌编辑
-        Route::post('/update', [GoodsBrandController::class, 'update']); // 商品品牌更新(新增)
-        Route::post('/destroy', [GoodsBrandController::class, 'destroy']); // 商品品牌删除
     });
 });
