2b45a4c18b28d6a54437f13687ef6ae77e71432f
李艳松 <walkerdistance@163.com>
2025-03-31 14:54:55 +0800

    change goods detail update api route js

--- resources/manage/api/goods.js
+++ resources/manage/api/goods.js
@@ -1,8 +1,10 @@
 import $http from '../utils/http'
 
+/* goods category api interface start */
 export function categoryIndex(data) {
     return $http.doGet('manage/goods/category', data)
 }
+
 export function categoryUpdate(data) {
     return $http.doPost('manage/goods/category/update', data)
 }
@@ -15,6 +17,7 @@ export function categoryDestroy(data) {
     return $http.doPost('manage/goods/category/destroy', data)
 }
 
+/* goods api interface start */
 export function goodsIndex(data) {
     return $http.doGet('manage/goods/info', data)
 }
@@ -27,10 +30,11 @@ export function goodsDetailInit(data) {
     return $http.doGet('manage/goods/info/edit', data)
 }
 
-export function getGoodsSkuTemplate() {
-    return $http.doGet('manage/goods/sku/template/small/index')
+export function goodsUpdate(data) {
+    return $http.doPost('manage/goods/info/update', data)
 }
 
+/* goods parameter template api interface start */
 export function getGoodsParameterTemplate() {
     return $http.doGet('manage/goods/parameter/template/small/index')
 }
@@ -45,4 +49,21 @@ export function goodsParameterTemplateUpdate(data) {
 
 export function goodsParameterTemplateDestroy(data) {
     return $http.doPost('manage/goods/parameter/template/destroy',data)
+}
+
+/* goods sku template api interface start */
+export function getGoodsSkuTemplate() {
+    return $http.doGet('manage/goods/sku/template/small/index')
+}
+
+export function goodsSkuTemplateStore(data) {
+    return $http.doPost('manage/goods/sku/template/store',data)
+}
+
+export function goodsSkuTemplateUpdate(data) {
+    return $http.doPost('manage/goods/sku/template/update',data)
+}
+
+export function goodsSkuTemplateDestroy(data) {
+    return $http.doPost('manage/goods/sku/template/destroy',data)
 }
