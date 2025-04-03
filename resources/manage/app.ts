018bbd1d0a9259237fd6e063381d86054d9fa09c
王欢 <hjsgwanghuan@163.com>
2025-04-03 17:00:20 +0800

    feat(上传)：增加公共上传组件

--- resources/manage/app.ts
+++ resources/manage/app.ts
@@ -5,6 +5,7 @@ import router from '@/router'
 import pinia from '@/store/index'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import FilesUpload from '@/components/common/FilesUpload.vue';
 import VueCookies from 'vue3-cookies'
 import mitt from 'mitt'
 import 'element-plus/dist/index.css'
@@ -22,6 +23,7 @@ const app = createApp(App);
 app.use(router);
 app.use(pinia)
 app.use(ElementPlus);
+app.component('FilesUpload', FilesUpload)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
