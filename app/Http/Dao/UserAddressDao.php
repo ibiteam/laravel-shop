dca45dc17ea798b474a491d3358e15268c721d8b
李艳松 <walkerdistance@163.com>
2025-04-07 10:11:55 +0800

    format user address dao

--- app/Http/Dao/UserAddressDao.php
+++ app/Http/Dao/UserAddressDao.php
@@ -1,27 +1,25 @@
 <?php
 
-
 namespace App\Http\Dao;
 
 use App\Exceptions\BusinessException;
 use App\Models\UserAddress;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
 
 class UserAddressDao
 {
     /**
-     * 获取用户地址列表
-     * @param int $user_id
-     * @param $keywords
-     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Support\Collection
+     * 获取用户地址列表.
      */
-    public function getAddrListsForUser(int $user_id, $keywords = null)
+    public function getAddrListsForUser(int $user_id, $keywords = null): EloquentCollection|Collection
     {
         $query = UserAddress::query()
-            ->select('id','user_id','recipient_name','recipient_phone','province','city','district','address_detail','is_default')
+            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
             ->whereUserId($user_id)
-            ->orderBy('is_default', 'desc')
-            ->orderBy('id', 'desc')
-            ->limit(60); // 最多返回 60 个地址
+            ->select(['id', 'user_id', 'recipient_name', 'recipient_phone', 'province', 'city', 'district', 'address_detail', 'is_default'])
+            ->orderByDesc('is_default')
+            ->orderByDesc('id');
 
         if ($keywords) {
             if (is_numeric($keywords)) {
@@ -35,54 +33,39 @@ class UserAddressDao
             }
         }
 
-        $data = $query->get()->map(function (UserAddress $item) use ($keywords) {
-            $addr = app(RegionDao::class)->getRegionName([$item->province, $item->city, $item->district]);
-            $item->province_name = $addr[0]['name'] ?? '';
-            $item->city_name = $addr[1]['name'] ?? '';
-            $item->district_name = $addr[2]['name'] ?? '';
-            $item->recipient_phone = phone_hidden($item->recipient_phone);
-
-            return $item;
+        return $query->limit(60)->get()->map(function (UserAddress $item) {
+            return $this->userAddressFormat($item);
         });
-
-        return $data;
     }
 
     /**
-     * 获取地址详情
-     * @param $user_id
-     * @param $id
-     * @return array|mixed[]
+     * 获取地址详情.
+     *
      * @throws BusinessException
      */
-    public function show($user_id, $id)
+    public function show(int $user_id, int $id): array
     {
         $item = UserAddress::query()
-            ->select('id','user_id','recipient_name','recipient_phone','province','city','district','address_detail','is_default')
+            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
+            ->select(['id', 'user_id', 'recipient_name', 'recipient_phone', 'province', 'city', 'district', 'address_detail', 'is_default'])
             ->whereUserId($user_id)
             ->find($id);
 
         if (! $item) {
             throw new BusinessException('地址不存在，请检查参数');
         }
-        $addr = app(RegionDao::class)->getRegionName([$item->province, $item->city, $item->district]);
-        $item->province_name = $addr[0]['name'] ?? '';
-        $item->city_name = $addr[1]['name'] ?? '';
-        $item->district_name = $addr[2]['name'] ?? '';
-        $item->recipient_phone = phone_hidden($item->recipient_phone);
 
-        return $item->toArray();
+        return $this->userAddressFormat($item);
     }
 
-    public function resetDefault(int $user_id)
+    public function resetDefault(int $user_id): void
     {
         UserAddress::whereUserId($user_id)->whereIsDefault(UserAddress::DEFAULT)->update(['is_default' => UserAddress::NOT_DEFAULT]);
     }
 
     public function replaceAddressByRegionId($provinceId, $cityId, $districtId, $address): string
     {
-        [$province,$city,$district] = app(RegionDao::class)->getRegionName([$provinceId, $cityId, $districtId])
-            ->pluck('region_name');
+        [$province,$city,$district] = app(RegionDao::class)->getRegionName([$provinceId, $cityId, $districtId])->pluck('region_name');
 
         return $this->replaceAddress($province, $city, $district, $address);
     }
@@ -94,5 +77,22 @@ class UserAddressDao
         }
 
         return $address;
+    }
+
+    /**
+     * 格式化地址数据.
+     */
+    private function userAddressFormat(UserAddress $user_address): array
+    {
+        return [
+            'id' => $user_address->id,
+            'recipient_name' => $user_address->recipient_name,
+            'recipient_phone' => $user_address->recipient_phone,
+            'province' => $user_address->regionProvince?->name,
+            'city' => $user_address->regionCity?->name,
+            'district' => $user_address->regionDistrict?->name,
+            'address_detail' => $user_address->address_detail,
+            'is_default' => $user_address->is_default,
+        ];
     }
 }
