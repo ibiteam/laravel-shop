026b8221f27e886be6c5d782c3947ec6c0c3d3b2
李艳松 <walkerdistance@163.com>
2025-04-07 10:32:34 +0800

    add get info by user address dao

--- app/Http/Dao/UserAddressDao.php
+++ app/Http/Dao/UserAddressDao.php
@@ -44,16 +44,43 @@ class UserAddressDao
      */
     public function show(int $user_id, int $id): array
     {
-        $item = UserAddress::query()
-            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
-            ->whereUserId($user_id)
-            ->find($id);
+        $item = $this->getUserAddressById($user_id, $id);
 
         if (! $item) {
             throw new BusinessException('地址不存在，请检查参数');
         }
 
         return $this->userAddressFormat($item);
+    }
+
+    /**
+     * 获取地址详情-收货地址ID或默认地址或第一条地址
+     */
+    public function showByIdOrDefault(int $user_id, int $id): array
+    {
+        if ($id > 0) {
+            // 尝试通过ID获取地址
+            $user_address = $this->getUserAddressById($user_id, $id);
+
+            if ($user_address instanceof UserAddress) {
+                return $this->userAddressFormat($user_address);
+            }
+        }
+
+        // 尝试获取默认地址
+        $user_address = $this->getUserAddressByDefault($user_id);
+
+        if ($user_address instanceof UserAddress) {
+            return $this->userAddressFormat($user_address);
+        }
+
+        $user_address = $this->getFirstUserAddress($user_id);
+
+        if ($user_address instanceof UserAddress) {
+            return $this->userAddressFormat($user_address);
+        }
+
+        return [];
     }
 
     public function resetDefault(int $user_id): void
@@ -75,6 +102,41 @@ class UserAddressDao
         }
 
         return $address;
+    }
+
+    /**
+     * 获取当前用户的用户地址
+     */
+    private function getUserAddressById(int $user_id, int $id): ?UserAddress
+    {
+        return UserAddress::query()
+            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
+            ->whereUserId($user_id)
+            ->whereId($id)
+            ->first();
+    }
+
+    /**
+     * 获取当前用户默认地址.
+     */
+    private function getUserAddressByDefault(int $user_id): ?UserAddress
+    {
+        return UserAddress::query()
+            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
+            ->whereUserId($user_id)
+            ->whereIsDefault(UserAddress::DEFAULT)
+            ->first();
+    }
+
+    /**
+     * 获取当前用户的第一个地址.
+     */
+    private function getFirstUserAddress(int $user_id): ?UserAddress
+    {
+        return UserAddress::query()
+            ->with(['regionProvince:id,name', 'regionCity:id,name', 'regionDistrict:id,name'])
+            ->whereUserId($user_id)
+            ->first();
     }
 
     /**
