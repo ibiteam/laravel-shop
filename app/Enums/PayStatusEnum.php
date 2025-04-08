b3825b7c7ab615e16fc5c44a67c14432ef3d62a2
李艳松 <walkerdistance@163.com>
2025-04-08 16:49:03 +0800

    add order other enum

---
+++ app/Enums/PayStatusEnum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+enum PayStatusEnum: int
+{
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::PAY_WAIT => '未支付',
+            self::PAYED => '已支付',
+        };
+    }
+
+    public static function getLabelBySource(string $source): string
+    {
+        return self::formSource($source)->getLabel();
+    }
+
+    public static function formSource(?int $source): self
+    {
+        return match ($source) {
+            0 => self::PAY_WAIT,
+            1 => self::PAYED,
+        };
+    }
+    case PAY_WAIT = 0; // 未支付
+    case PAYED = 1; // 已支付
+}
---
+++ app/Enums/ShippingStatusEnum.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enums;
+
+enum ShippingStatusEnum: int
+{
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::UNSHIPPED => '未确认',
+            self::SHIPPED => '已确认',
+            self::RECEIVED => '已取消',
+        };
+    }
+
+    public static function getLabelBySource(string $source): string
+    {
+        return self::formSource($source)->getLabel();
+    }
+
+    public static function formSource(?int $source): self
+    {
+        return match ($source) {
+            0 => self::UNSHIPPED,
+            1 => self::SHIPPED,
+            2 => self::RECEIVED,
+        };
+    }
+    case UNSHIPPED = 0; // 未发货
+    case SHIPPED = 1; // 已发货
+    case RECEIVED = 2; // 已收货
+}
