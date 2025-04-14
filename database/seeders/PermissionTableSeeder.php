823104b34e6db835dc59b61685d9003b5f3721f5
李艳松 <walkerdistance@163.com>
2025-04-14 19:45:00 +0800

    change permission table seeder

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -77,6 +77,7 @@ class PermissionTableSeeder extends Seeder
         $this->addPermission('支付管理', Permission::BASIC_PAYMENT_MANAGE, 98, Permission::IS_LEFT_NAV, 'Menu', Permission::MODULE_SET);
         $this->addPermission('支付方式', Permission::MANAGE_PAYMENT_INDEX, 99, Permission::IS_LEFT_NAV, '', Permission::BASIC_PAYMENT_MANAGE);
         $this->addPermission('支付方式编辑', Permission::MANAGE_PAYMENT_UPDATE, 0, Permission::NOT_IS_LEFT_NAV, '', Permission::BASIC_PAYMENT_MANAGE);
+        $this->addPermission('交易记录', Permission::MANAGE_TRANSACTION_INDEX, 98, Permission::IS_LEFT_NAV, '', Permission::BASIC_PAYMENT_MANAGE);
 
         $this->addPermission('网站管理', Permission::WEBSITE_MANAGE, 1, Permission::IS_LEFT_NAV, 'Menu', Permission::MODULE_SET);
         $this->addPermission('移动端装修', Permission::MANAGE_APP_DECORATION, 1, Permission::IS_LEFT_NAV, '', Permission::WEBSITE_MANAGE);
@@ -121,7 +122,7 @@ class PermissionTableSeeder extends Seeder
 
         $this->addPermission('发货管理', Permission::ORDER_DELIVERY_MANAGE, 0, Permission::IS_LEFT_NAV, 'Menu', Permission::MODULE_ORDER);
         $this->addPermission('发货列表', Permission::MANAGE_ORDER_DELIVERY_INDEX, 0, Permission::IS_LEFT_NAV, '', Permission::ORDER_DELIVERY_MANAGE);
-        $this->addPermission('发货导入|删除', Permission::MANAGE_ORDER_DELIVERY_UPDATE, 0, Permission::IS_LEFT_NAV, '', Permission::ORDER_DELIVERY_MANAGE);
+        $this->addPermission('发货导入|删除', Permission::MANAGE_ORDER_DELIVERY_UPDATE, 0, Permission::NOT_IS_LEFT_NAV, '', Permission::ORDER_DELIVERY_MANAGE);
     }
 
     private function addArticlePermission(): void
