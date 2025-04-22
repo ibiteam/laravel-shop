bbacddf81930fc82d8104bf69b445b87efa969b7
张未东 <boyphp@163.com>
2025-04-22 15:17:35 +0800

    feat:商品分类

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Api\BaseController;
 use App\Http\Dao\CategoryDao;
+use Illuminate\Support\Facades\Log;
 
 /**
  * 商品分类.
@@ -13,8 +14,9 @@ class CategoryController extends BaseController
     public function index(CategoryDao $category_dao)
     {
         try {
-            return $this->success($category_dao->getGoodsCategory());
+            return $this->success($category_dao->getTreeList());
         } catch (\Throwable $throwable) {
+            Log::error('获取商品分类异常'.$throwable->getMessage());
             return $this->error('获取商品分类异常');
         }
     }
