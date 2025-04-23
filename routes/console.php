c8464fe51a7d3d00effbd14299dc445c7515133f
李艳松 <walkerdistance@163.com>
2025-04-23 20:09:12 +0800

    change console

--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,10 @@
 <?php
 
 use App\Http\Dao\AccessStatisticDao;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 // 访问统计
 Schedule::call(function () {
     app(AccessStatisticDao::class)->statistic();
 })->dailyAt('08:30');
+// 删除过期的token
+Schedule::command('sanctum:prune-expired --hours=24')->daily();
